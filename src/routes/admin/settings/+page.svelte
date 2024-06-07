19a583488723965a693804243f0b897976b3063d
nikdorbaz <nikdorbaznikdorbaz@Noutbuk-Boris.local>
2024-06-07 17:42:31 +0300

    wip

--- src/routes/admin/settings/+page.svelte
+++ src/routes/admin/settings/+page.svelte
@@ -4,6 +4,8 @@
   import { createNamespace } from '$lib/utils';
 
   const [navName, bemNav] = createNamespace('nav');
+  const [boxName, bemBox] = createNamespace('box');
+  const [layoutName, bemLayout] = createNamespace('layout');
 
   /** @todo remove later */
   const menu = {
@@ -41,33 +43,38 @@
   };
 </script>
 
-<nav class={navName}>
-  <ul class={bemNav('items')}>
-    {#each Object.entries(menu) as [key, item]}
-      {#if item.name !== ''}
-        <li class={bemNav('item')}>
-          <a href={key} class={bemNav('link', { active: item?.active || false })}>
-            <Icon />
-            <Text>{item.name}</Text>
-          </a>
-        </li>
-      {/if}
-    {/each}
-  </ul>
-</nav>
+<div class={layoutName}>
+  <nav class={navName}>
+    <ul class={bemNav('items')}>
+      {#each Object.entries(menu) as [key, item]}
+        {#if item.name !== ''}
+          <li class={bemNav('item')}>
+            <a href="#{key}" class={bemNav('link', { active: item?.active || false })}>
+              <Icon />
+              <Text>{item.name}</Text>
+            </a>
+          </li>
+        {/if}
+      {/each}
+    </ul>
+  </nav>
+  <div class={boxName}>
+    <Text as="h1">Общие</Text>
+  </div>
+</div>
 
 <style lang="scss">
   :root {
-    --mf-primary-color: #005bc4;
-    --mf-color-bg: #f1f2f4;
+    --mf-link-main-color: #ecedee;
+    --mf-link-secondary-color: #a1a1aa;
     --mf-radius-1: 0.5rem;
     --mf-spacing-1: 0.5rem;
+    --mf-gap-1: 1rem;
   }
 
   .mf-nav {
     display: flex;
     width: 18em;
-    background-color: white;
     border-radius: var(--mf-radius-1);
     padding: var(--mf-spacing-1);
   }
@@ -84,17 +91,38 @@
     gap: var(--mf-spacing-1);
     border-radius: var(--mf-radius-1);
     padding: var(--mf-spacing-1);
+    color: var(--mf-link-secondary-color);
 
     &:hover {
-      background-color: var(--mf-color-bg);
+      background-color: var(--mf-secondary-bg);
     }
 
     &--active {
-      background-color: var(--mf-primary-color);
+      background-color: var(--mf-secondary-bg);
+      color: var(--mf-link-main-color);
 
       &:hover {
-        background-color: var(--mf-primary-color);
+        background-color: var(--mf-secondary-bg);
       }
     }
+  }
+
+  /** Layout el */
+  .mf-layout {
+    display: flex;
+    justify-content: center;
+    padding: 1rem 2rem;
+    gap: 1rem;
+  }
+
+  /** Box el */
+  .mf-box {
+    display: flex;
+    gap: var(--mf-gap-1);
+    width: 46.75rem;
+  }
+
+  :global(.mf-text) {
+    color: var(--mf-link-main-color);
   }
 </style>

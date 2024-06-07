07a5ce31cb126d7eccc0f3e6e5731fdbe0c3d13b
tmvc03 <to.boris03@gmail.com>
2024-06-07 14:29:18 +0300

    feat(style/text): impl font-size & font-weight

--- src/routes/id/login/+page.svelte
+++ src/routes/id/login/+page.svelte
@@ -1,3 +1,21 @@
-hello world
+<form action="">
+  <span>logo</span>
+  <h1>mf ID</h1>
+  <p>Please confirm your country code and enter your phone number.</p>
+  <h1 class="text-3xl font-semibold">hello world</h1>
 
-<span class="text-3xl">hello world</span>
+  <p>
+    <label for="loginEmail">Email</label>
+    <input type="email" id="loginEmail" placeholder="Enter your email" />
+  </p>
+
+  <p>
+    <label for="loginPassword">Password</label>
+    <input type="password" id="loginPassword" placeholder="Enter your password" />
+  </p>
+
+  <a href="/id/forgot-password">Забыл пароль?</a>
+
+  <button type="submit">Log in</button>
+  <a href="/id/sign-up">Create an account</a>
+</form>

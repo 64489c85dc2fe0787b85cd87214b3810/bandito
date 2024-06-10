0729bfa1a539ad22dd34b1efe4a012f9cdcaa92e
tmvc03 <to.boris03@gmail.com>
2024-06-10 21:59:38 +0300

    push

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script>
   import '../scss/bandito.scss';
+  import '../scss2/test.scss';
+
   let { children } = $props();
 </script>
 
 {@render children()}
+
+<input type="test" />
+
+<ul>
+  <li><a href="#">test</a></li>
+  <li><a href="#">test</a></li>
+  <li><a href="#">test</a></li>
+</ul>

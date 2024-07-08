a9c8dda3d04e155c88084554654c307e18ebe1e1
tmvc03 <to.boris03@gmail.com>
2024-07-09 02:08:16 +0300

    chore: update version

--- src/lib/background.svelte
+++ src/lib/background.svelte
@@ -1,6 +1,6 @@
 <script lang="ts" context="module">
   export type BackgroundProps = {
-    color: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+    color?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
   };
 
   const colors = [
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+  import { Background } from '$lib';
   import '../scss/_index.scss';
 
   let { children } = $props();
 </script>
+
+<Background color={0} />
 
 <svelte:head>
   <title>Bandito</title>

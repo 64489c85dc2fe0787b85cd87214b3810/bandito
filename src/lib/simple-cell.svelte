64d6c4d2640873e4db58d29184af72ee91b29da5
tmvc03 <to.boris03@gmail.com>
2024-07-11 18:43:49 +0300

    chore: update version

--- src/lib/simple-cell.svelte
+++ src/lib/simple-cell.svelte
@@ -2,21 +2,26 @@
   import { cl, createNamespace } from './utils';
   import type { DefaultProps } from './types';
   import type { Snippet } from 'svelte';
+  import type { CellIconProps } from './cell-icon.svelte';
 
   export type SimpleCellProps = DefaultProps & {
     children: Snippet;
     image?: Snippet;
     after?: Snippet;
+    icon?: CellIconProps;
   };
 
   const [ns, bem] = createNamespace('cell');
 </script>
 
 <script lang="ts">
+  import CellIcon from './cell-icon.svelte';
+
   let {
     children,
     image,
     after,
+    icon,
     class: className,
     tag = 'div',
     ...restProps
@@ -26,6 +31,9 @@
 <svelte:element this={tag} class={cl(ns, className)} {...restProps}>
   {#if image}
     <span class={bem('image')}>{@render image()}</span>
+  {/if}
+  {#if icon}
+    <span class={bem('image')}><CellIcon {...icon} /></span>
   {/if}
   <span class={bem('content')}>
     <span>{@render children()}</span>
